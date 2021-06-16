1199212d5b730fc42d0c77bc583cd1c16abeefe7
Roge <orogelioangel@gmail.com>
2021-06-15 22:26:47 -0500

    create directory for php version

---
+++ php/partials/navbar.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li><a href="">Inicio</a></li>
+    </ul>
+    <ul>
+        <li><a href="">¿Quienes somos?</a></li>
+    </ul>
+    <ul>
+        <li><a href="">Contacto</a></li>
+    </ul>
+</nav>
